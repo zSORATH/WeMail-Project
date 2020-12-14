dbbf95beb3abd007a5ccc287a9d987db51d25b7f
Jeppe S <jepp5220@gmail.com>
2020-12-14 17:32:03 +0100

    Mainly refactoring
    Moved email send function into its own class

--- EmailProject-Main-WORKING/EmailProject-main/VisualStudioProject/PInvokeTest/Form1.cs
+++ EmailProject-Main-WORKING/EmailProject-main/VisualStudioProject/PInvokeTest/Form1.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 using System.Runtime.InteropServices;
 namespace PInvokeTest
 {
+
     public partial class WEMAIL : Form
     {
         public WEMAIL()
@@ -25,7 +26,6 @@ namespace PInvokeTest
 
         private void button1_Click(object sender, EventArgs e)
         {
-            pV.pinvoke();
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -45,65 +45,88 @@ namespace PInvokeTest
 
         private void sendButton_Click(object sender, EventArgs e)
         {
-            try
-            {
-                string server = "mail.smtp2go.com"; //Current SMTP server. Free
+           
                 string to = reciptxtbox.Text; //Takes user input for recipient
                 string from = "TestingPurposes@wemail.com"; //Current Sender. Needs changing to be personal to each account
+
                 MailAddress Sender = new MailAddress(from); //Its possible to do MailAdress(from,"displayname"). Could be usefull ?
                 MailAddress Recipient = new MailAddress(to);
                 MailMessage message = new MailMessage(Sender, Recipient);
-                string domain = to.Substring(to.LastIndexOf('@') + 1);
-                message.Subject = subjecttxtbox.Text;
-                message.Body = @messagetxtbox.Text;
-                char DL = '';
+
+                string subject = subjecttxtbox.Text;
+                string text = @messagetxtbox.Text;
+                string domain = to.Substring(to.LastIndexOf('@') + 1); //Takes everything to the right of @
+
                 if (domain.Equals("wemail.com", StringComparison.OrdinalIgnoreCase))
                 {
-                    //MessageBox.Show("You're sending to a Wemail account", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    const int PORT_NO = 5000;
-                    const string LOCALHOST = "127.0.0.1";
-                    TcpClient tcpclient = new TcpClient(LOCALHOST, PORT_NO);
-                    NetworkStream nwStream = tcpclient.GetStream();
-                    //sndr, timestamp, subject, txt
-                    var TTS = from + DL + DateTime.Today.ToShortDateString() + DL + message.Subject + DL + message.Body+ DL;//Improvement is to use stringbuilder append() as it is faster
-                    byte[] bytesToSend = ASCIIEncoding.UTF8.GetBytes(TTS);
-                    Console.WriteLine("Sending : " + message.Body);
-                    nwStream.Write(bytesToSend, 0, bytesToSend.Length);
-
-                    //---read back the text---
-                    byte[] bytesToRead = new byte[tcpclient.ReceiveBufferSize];
-                    int bytesRead = nwStream.Read(bytesToRead, 0, tcpclient.ReceiveBufferSize);
-                    Console.WriteLine("Received : " + Encoding.UTF8.GetString(bytesToRead, 0, bytesRead));
-                    Console.ReadLine();
-                    tcpclient.Close();
+                    SendMail.Wemailtransfer(Sender,Recipient,subject,text);
                 }
                 else
                 {
-
-                    SmtpClient client = new SmtpClient(server);
-                    client.Send(message);
-                    MessageBox.Show("Email Sent!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    SendMail.Regular(Sender,Recipient,subject,text);
                 }
-            }
-            catch (Exception ex)
-            {
-                MessageBoxButtons buttons = MessageBoxButtons.RetryCancel;
-                MessageBox.Show(ex.ToString(), "error", buttons, MessageBoxIcon.Warning);
-            }
         }
         private void label4_Click(object sender, EventArgs e)
         {
 
         }
     }
-    public class pV
+    public class SendMail
     {
-        public static void pinvoke()
+        static string server = "mail.smtp2go.com"; //Current SMTP server. Coupled to IP
+        public static void Regular(MailAddress Sender, MailAddress Recipient, string subject, string text)
         {
-            print_line("Hello!");
-            Console.WriteLine("This is C#");
+            MailMessage message = new MailMessage(Sender, Recipient);
+            SmtpClient client = new SmtpClient(server);
+            client.Send(message);
+            MessageBox.Show("Email Sent!", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
-        [DllImport("NativeLib.dll")]
-        private static extern void print_line(string str);
+        public static void Wemailtransfer(MailAddress Sender,MailAddress Recipient, string subject, string text)
+        {
+            char DL = '';
+            try
+            {
+                MailMessage message = new MailMessage(Sender, Recipient);
+                message.Subject = subject;
+                message.Body = text;
+                //MessageBox.Show("You're sending to a Wemail account", "Success!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                const int PORT_NO = 5000;
+                const string LOCALHOST = "127.0.0.1";
+                TcpClient tcpclient = new TcpClient(LOCALHOST, PORT_NO);
+                NetworkStream nwStream = tcpclient.GetStream();
+                //sndr, timestamp, subject, txt
+                var TTS = Sender.Address + DL + DateTime.Today.ToShortDateString() + DL + message.Subject + DL + message.Body + DL;//Improvement is to use stringbuilder append() as it is faster
+                byte[] bytesToSend = ASCIIEncoding.UTF8.GetBytes(TTS);
+                Console.WriteLine("Sending : " + message.Body);
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+                //---read back the text---
+                byte[] bytesToRead = new byte[tcpclient.ReceiveBufferSize];
+                int bytesRead = nwStream.Read(bytesToRead, 0, tcpclient.ReceiveBufferSize);
+                Console.WriteLine("Received : " + Encoding.UTF8.GetString(bytesToRead, 0, bytesRead));
+                Console.ReadLine();
+                tcpclient.Close();
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler.SendMailException(ex);
+            }
+        }
+        public static void Forward()
+        {
+            //
+        }
+        public static void Reply()
+        {
+            //
+        }
+    }
+    public class ExceptionHandler
+    {
+        public static void SendMailException(Exception ex)
+        {
+            MessageBoxButtons buttons = MessageBoxButtons.RetryCancel;
+            MessageBox.Show(ex.ToString(), "error", buttons, MessageBoxIcon.Warning);
+        }
     }
 }
